9662eea797429da8fa080f95e026337611d52dc2
LastMinuteHero <hks_email@yahoo.com>
2018-05-11 15:55:57 +0200

    falscher commit davor/ Korrektur

--- redis uebung/src/JsonReader.java
+++ redis uebung/src/JsonReader.java
@@ -1,4 +1,72 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import redis.clients.jedis.Jedis;
 
 public class JsonReader {
+	
+	private static final String filePath = "C:\\Users\\hks_e\\Desktop\\business.json";
+	
+	public static void main(String[] args) {
+
+
+		try {
+			// read the json file
+			FileReader reader = new FileReader(filePath);
+
+			JSONParser jsonParser = new JSONParser();
+			JSONArray jsonArray = (JSONArray) jsonParser.parse(reader);
+			
+			Jedis jedis = new Jedis("localhost");
+
+			// get a String from the JSON object
+			for (int i=0; i<jsonArray.size(); i++) {
+				JSONObject jsonObject = (JSONObject)jsonArray.get(i);
+				String businessId = (String) jsonObject.get("business_id");
+				jedis.hmset(businessId, jsonObject)
+				
+			}
+
+//			// get a number from the JSON object
+//			long id =  (long) jsonObject.get("id");
+//			System.out.println("The id is: " + id);
+//
+//			// get an array from the JSON object
+//			JSONArray lang= (JSONArray) jsonObject.get("languages");
+//			
+//			// take the elements of the json array
+//			for(int i=0; i<lang.size(); i++){
+//				System.out.println("The " + i + " element of the array: "+lang.get(i));
+//			}
+//			Iterator i = lang.iterator();
+//
+//			// take each value from the json array separately
+//			while (i.hasNext()) {
+//				JSONObject innerObj = (JSONObject) i.next();
+//				System.out.println("language "+ innerObj.get("lang") + 
+//						" with level " + innerObj.get("knowledge"));
+//			}
+//			// handle a structure into the json object
+//			JSONObject structure = (JSONObject) jsonObject.get("job");
+//			System.out.println("Into job structure, name: " + structure.get("name"));
+
+		} catch (FileNotFoundException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} catch (ParseException ex) {
+			ex.printStackTrace();
+		} catch (NullPointerException ex) {
+			ex.printStackTrace();
+		}
+
+	}
 
 }
--- redis uebung/src/RedisConnect.java
+++ redis uebung/src/RedisConnect.java
@@ -1,4 +1,27 @@
+import redis.clients.jedis.Jedis;
+
+//run redis-server in terminal / cmd to start database server
 
 public class RedisConnect {
+
+	public void connectRedis() {
+		try{
+			Jedis jedis = new Jedis("localhost");
+			System.out.println("Connection Successful");
+			System.out.println("Server ping" + jedis.ping());
+			
+			// random
+			//System.out.println("List push key value " + jedis.lpush("Max", "Karl"));
+			//System.out.println("List pop Max: " + jedis.lpop("Max"));
+			
+			
+			
+			
+		} catch(Exception e)
+		{
+			System.out.println(e);
+		}
+
+	}
 
 }
